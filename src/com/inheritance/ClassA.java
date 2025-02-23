465026700a2b6394955352fc4a2cef38b78ca87a
dharan121 <dharan94567@gmil.com>
2025-02-23 13:52:04 +0530

    general code

---
+++ src/com/inheritance/ClassA.java
@@ -0,0 +1,11 @@
+package com.inheritance;
+
+public class ClassA {
+	public void EmpName() {
+		System.out.println("EmpName is dharan");
+	}
+	public void EmpId() {
+		System.out.println("EmpId is 8435");
+	}
+
+}
---
+++ src/com/inheritance/ClassB.java
@@ -0,0 +1,19 @@
+package com.inheritance;
+
+public class ClassB extends ClassA {
+	
+	public void EmpNum() {
+		System.out.println("EmpNum is 8353");
+	}
+	public static void main(String[]agrs) {
+		
+		ClassB child=new ClassB();
+		
+		child.EmpNum();
+		
+		child.EmpName();
+		
+		child.EmpId();
+	}
+
+}
